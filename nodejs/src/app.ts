d62e4a71b9bbff8d680755b235b4599cf0fba3ed
SUDA Mikihrio <s@sudame.net>
2021-08-21 12:32:18 +0900

    add: drop率を9割に戻す

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1129,8 +1129,7 @@ app.post(
     res
   ) => {
     // TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-    // sudame: もともと0.9だったがクエリの発行数を3割削減したので3割削減する
-    const dropProbability = 0.3
+    const dropProbability = 0.9
     if (Math.random() <= dropProbability) {
       console.warn("drop post isu condition request")
       return res.status(202).send()
