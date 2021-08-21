19db717a7131f8981655b751435e1338473b8a60
wakkihaya <wakkihaya@gmail.com>
2021-08-21 11:19:18 +0900

    add: N+1 comment

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -356,6 +356,7 @@ app.get("/api/isu", async (req, res) => {
 
     await db.beginTransaction();
 
+    //TODO: N+1
     const [isuList] = await db.query<Isu[]>(
       "SELECT * FROM `isu` WHERE `jia_user_id` = ? ORDER BY `id` DESC",
       [jiaUserId]
@@ -1025,6 +1026,7 @@ app.get("/api/trend", async (req, res) => {
 
     const trendResponse: TrendResponse[] = [];
 
+    //TODO: N+1
     for (const character of characterList) {
       const [isuList] = await db.query<Isu[]>(
         "SELECT * FROM `isu` WHERE `character` = ?",
